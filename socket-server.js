79fbf9f1a07adaa9914c11a35d931b55e22ca7f1
Oleksandr Andrieiev <kuricyak@gmail.com>
2018-06-06 18:03:12 +0300

    added socket.io, ot - sync with unique rooms

---
+++ socket-server.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var socketIO = require('socket.io');
+var ot = require('ot');
+var roomList = {};
+
+module.exports = function(server) {
+    var str = "This is Markdown";
+    var io = socketIO(server);
+    io.on('connection', function (socket) {
+        socket.on('joinRoom', function(data) {
+            if(!roomList[data.room]) {
+                var socketIOServer = new ot.EditorSocketIOServer(str, [], data.room, function(socket, cb){
+                    cb(true);
+                });
+                roomList[data.room] = socketIOServer;
+            }
+            roomList[data.room].addClient(socket);
+            roomList[data.room].setName(socket, data.username);
+
+            socket.room = data.room;
+            socket.join(data.room);
+        });
+
+        socket.on('chatMessage', function(data){
+            io.to(socket.room).emit('chatMessage', data);
+        });
+
+        socket.on('disconnect', function(data){
+            socket.leave(socket.room);
+        });
+    })
+}
