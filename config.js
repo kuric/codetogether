69febb51f230c33bda33bb722ca58aebb5aae38e
Oleksandr Andrieiev <kuricyak@gmail.com>
2018-06-26 14:28:23 +0300

    hotfixes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
-var url = process.env.MONGOLAB_URI;
-// var url = "mongodb://localhost:27017/codetogether";
+// var url = process.env.MONGOLAB_URI;
+var url = "mongodb://localhost:27017/codetogether";
 module.exports = {
     mailer: {
         service: 'Gmail',
